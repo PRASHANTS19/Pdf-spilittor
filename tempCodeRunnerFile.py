190ba20f4eb95bc24b0cdf15f4e406ea0bf3a83d
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-01-13 18:57:18 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+@app.route("/success",method=["POST"])
+# def success():
+#     global st
+#     global en
+#     global file
+#     st=int(request.form['start'])
+#     en=int(request.form['end'])
+#     f=request.files['file']
+#     file=f.filename
+#     f.save(file)
+#     return render_template("success.html",start=st,end=en,name=file)
